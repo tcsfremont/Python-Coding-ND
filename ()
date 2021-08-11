e99f0cdf74163c67a7efe2e7787fe28227841393
georgegeiwitzcesana <georgegeiwitzcesana@gmail.com>
2021-08-11 10:23:17 -0700

    cleaning data

---
+++ ()
@@ -0,0 +1,4 @@
+from sys import argv as arguments
+
+from_filename = arguments[1]
+print(from_filename)
--- frequency.py
+++ frequency.py
@@ -1,16 +1,50 @@
 import sys
 
+ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
+
+args = sys.argv[1:]
+
+for arg in args:
+
+    print(arg)
+
 text = ' '.join(sys.argv[1:])
+lowercase_letter = text[0].lower()
+
+print(lowercase_letter)
+
+lowercase_text = [letter.lower() for letter in text]
+
+print(lowercase_text)
+
+lowercase_text = ''.join(lowercase_text)
+print(lowercase_text)
 frequencies = {}
 
-for letter in text:
+lowercase_text = [letter if letter in ALPHABET else '' for letter in lowercase_text]
 
-    if letter not in frequencies:
+print(lowercase_text)
 
-        frequencies[letter] = 1
+lowercase_text = ''.join(lowercase_text)
 
-    else:
+for letter in ALPHABET:
+    frequencies[letter] = 0
 
-        frequencies[letter] += 1
+for letter in lowercase_text:
 
-print "frequencies"
+
+    frequencies[letter] += 1
+
+print (frequencies)
+
+for letter in ALPHABET:
+    
+    count = frequencies[letter]
+    
+    print(letter, end='')
+
+    for c in range (count):
+        
+        print('|', end='')
+
+    print()
